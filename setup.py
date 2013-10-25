685e861774b3e325307ebcea7f7a72b27b6ad688
David Daniel <David.Daniel@dealertrack.com>
2013-10-24 23:59:31 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+	name="Transformer",
+	version="0.0.1",
+	author="David Daniel",
+	author_email="David.Daniel@dealertrack.com",
+	description=("Transforms provided input to an output, based on the transform."),
+	license="BSD",
+	keywords="transformer xslt xml xslt transform",
+	packages=find_packages(),
+	long_description=read('README.md'),
+	entry_points={
+		'console_scripts' : [
+			'transformer = transformer.runner:main'
+		]
+	},
+	install_requires=['lxml==3.2.1']
+)
